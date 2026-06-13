7c3b877d7f7a490fe9e0f2b02d97982984fdf289
aphyrince <dkswjdgh01@naver.com>
2026-06-13 20:27:16 +0900

    튜토리얼 챕터3 완료

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+const PlaceholderImage = require('@/assets/images/images/background-image.png');
 
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home Screen.</Text>
-            <Link href='/about' style={styles.button}>
-                Go to About screen
-            </Link>
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button label='Choose a photo' theme='primary' />
+                <Button label='Use this photo' />
+            </View>
         </View>
     );
 }
@@ -17,14 +23,17 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#25292e',
         alignItems: 'center',
+        gap: 12,
+    },
+
+    imageContainer: {
+        flex: 1,
+        paddingTop: 28,
+        minHeight: 440,
+    },
+    footerContainer: {
+        flex: 1,
         justifyContent: 'center',
-    },
-    text: {
-        color: '#fff',
-    },
-    button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: '#fff',
+        alignItems: 'center',
     },
 });
