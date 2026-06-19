fb11dc4c2b13e7eb373254ac772fea479cbbb327
aphyrince <dkswjdgh01@naver.com>
2026-06-19 19:40:09 +0900

    expo docs 실습용 ExpoStudy 프로젝트 생성.

--- taro_Clone/app/login.tsx
+++ taro_Clone/app/login.tsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import React from 'react';
+import { useRouter } from 'expo-router';
 
-export default function login() {
+export default function Login() {
+    const router = useRouter();
+
     return (
         <View>
             <Text>login</Text>
+            <Button title='Go to About' onPress={() => router.navigate('/index')} />
         </View>
     );
 }
