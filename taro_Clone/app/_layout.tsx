d5f0d5a2c8dd47780fcf993593ec94126e138c67
aphyrince <dkswjdgh01@naver.com>
2026-06-20 17:44:07 +0900

    루트레이아웃 설정

--- taro_Clone/app/_layout.tsx
+++ taro_Clone/app/_layout.tsx
@@ -3,11 +3,11 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name='login' />
-            <Stack.Screen name='signIn' />
             <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-            <Stack.Screen name='cardSelection' />
-            <Stack.Screen name='result' />
+            <Stack.Screen name='login' options={{ title: 'Login' }} />
+            <Stack.Screen name='signIn' options={{ title: 'sign in' }} />
+            <Stack.Screen name='cardSelection' options={{ headerShown: false }} />
+            <Stack.Screen name='result' options={{ title: 'result' }} />
         </Stack>
     );
 }
