f2ecafa5c89d1463f458c63ce99137b8753a5482
aphyrince <dkswjdgh01@naver.com>
2026-06-23 20:40:55 +0900

    로그인 페이지 기능 구현. 이제 디자인 구현해야함.

--- taro_Clone/src/app/(auth)/login.tsx
+++ taro_Clone/src/app/(auth)/login.tsx
@@ -1,16 +1,38 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import React from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 import { useRouter } from 'expo-router';
+import { signIn } from '../../services/auth';
 
 export default function Login() {
     const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        const result = await signIn(email, password);
+        if (result.success === 200) router.replace('/(tabs)');
+        else Alert.alert('로그인 실패!');
+    };
 
     return (
         <View>
-            <Text>login</Text>
-            <TextInput />
-            <TextInput />
-            <Button title='Go to About' onPress={() => router.navigate('/(tabs)')} />
+            <Text>로그인</Text>
+            <TextInput
+                keyboardType='email-address'
+                autoCapitalize='none'
+                textContentType='emailAddress'
+                placeholder='your@email.com'
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                textContentType='password'
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry
+            />
+            <Button title='로그인' onPress={handleLogin} />
+            <Button title='회원가입' onPress={() => router.navigate('/signIn')} />
         </View>
     );
 }
