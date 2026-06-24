fa02eb3abc3b80fd09aefbf3fa6574de36675dc7
aphyrince <dkswjdgh01@naver.com>
2026-06-24 14:41:49 +0900

    메인화면 완성

--- taro_Clone/src/app/(tabs)/index.tsx
+++ taro_Clone/src/app/(tabs)/index.tsx
@@ -1,12 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { signOut } from '../../services/auth';
+import { useRouter } from 'expo-router';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 export default function Main() {
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        try {
+            await signOut();
+        } catch (error: any) {
+            alert(error.message);
+            Alert.alert(error.message);
+        }
+    };
+
     return (
-        <View>
-            <Text>Main</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>타로 앱</Text>
+            <Pressable style={styles.button} onPress={() => router.navigate('/cardSelection')}>
+                <Text>타로 점보기 시작</Text>
+            </Pressable>
+            <Pressable style={[styles.button, styles.link]} onPress={handleLogout}>
+                <Text>로그아웃</Text>
+            </Pressable>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.primary,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 700,
+        marginBottom: 24,
+        color: colors.text,
+        fontFamily: fonts.medium,
+    },
+    button: {
+        marginTop: 8,
+        backgroundColor: colors.third,
+    },
+    link: {
+        marginTop: 16,
+    },
+});
